ba1fdfbaa0283e5168c9636c37f7e0b1488f61d6
Qingyu Shang <2931013282@qq.com>
2023-02-15 22:56:07 +0800

    debug: fix some trival bugs in build and install scripts, refine scripts
    Signed-off-by: Qingyu Shang <2931013282@qq.com>

--- script/build_all.sh
+++ script/build_all.sh
@@ -7,6 +7,13 @@ fi
 # get hart number
 CPU_NUM=$(cat /proc/cpuinfo| grep "processor"| wc -l)
 
+mkdir -p $SECURE_WORLD_DIR/out/opensbi
+mkdir -p $SECURE_WORLD_DIR/out/uboot
+mkdir -p $SECURE_WORLD_DIR/out/normal_linux/kernel
+mkdir -p $SECURE_WORLD_DIR/out/normal_linux/driver
+mkdir -p $SECURE_WORLD_DIR/out/normal_linux/sdk
+mkdir -p $SECURE_WORLD_DIR/out/secure_linux
+
 # build fw_payload.bin.out
 cd $SECURE_WORLD_DIR/starfive-uboot
 make starfive_jh7100_visionfive_smode_defconfig ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu-
@@ -14,26 +21,57 @@ make u-boot.bin u-boot.dtb ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu-
 cd $SECURE_WORLD_DIR/starfive-penglai/opensbi-0.9
 make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- PLATFORM=generic FW_PAYLOAD_PATH=$SECURE_WORLD_DIR/starfive-uboot/u-boot.bin FW_FDT_PATH=$SECURE_WORLD_DIR/starfive-uboot/u-boot.dtb
 cd $SECURE_WORLD_DIR
-./script/fsz.sh starfive-penglai/opensbi-0.9build/platform/generic/firmware/fw_payload.bin out/fw_payload.bin.out
+./script/fsz.sh starfive-penglai/opensbi-0.9/build/platform/generic/firmware/fw_payload.bin fw_payload.bin.out
+mv starfive-penglai/opensbi-0.9/build/platform/generic/firmware/fw_payload.bin.out out/opensbi/
 
 # build normal linux
 cd $SECURE_WORLD_DIR/starfive-normal-linux
+# git checkout f81e6caa4728ff4fab86112a1c45bf031cd8f7d3
+# cp $SECURE_WORLD_DIR/package/brcmfmac43430-sdio.bin firmware/brcm/
 make CROSS_COMPILE=riscv64-linux-gnu- ARCH=riscv visionfive_defconfig
-make CROSS_COMPILE=riscv64-linux-gnu- ARCH=riscv dtbs
-mv arch/riscv/boot/dts/starfive/jh7100-starfive-visionfive-v1.dtb $SECURE_WORLD_DIR/out/visionfive-v1-uart-reserved-plic-nonet.dtb
-
-git checkout a6fbbbdf
-make CROSS_COMPILE=riscv64-linux-gnu- ARCH=riscv visionfive_defconfig
+# make CROSS_COMPILE=riscv64-linux-gnu- ARCH=riscv dtbs
 make CROSS_COMPILE=riscv64-linux-gnu- ARCH=riscv -j$CPU_NUM
-mv arch/riscv/boot/Image.gz $SECURE_WORLD_DIR/out/fedora_with_modules.gz
+mv arch/riscv/boot/Image.gz $SECURE_WORLD_DIR/out/normal_linux/kernel/fedora_with_modules.gz
+mv arch/riscv/boot/dts/starfive/jh7100-starfive-visionfive-v1.dtb $SECURE_WORLD_DIR/out/normal_linux/kernel/visionfive-v1-uart-reserved-plic-nonet.dtb
 
 # build user-level sdk and driver for secure-linux installation
 cd $SECURE_WORLD_DIR/starfive-penglai
-cd penglai-selinux-driver && make && cp penglai_linux.ko $SECURE_WORLD_DIR/out/ && cd ..
-cd penglai-selinux-sdk && make && cp host/host $SECURE_WORLD_DIR/out/
+cd penglai-selinux-driver && make && cp penglai_linux.ko $SECURE_WORLD_DIR/out/normal_linux/driver/ && cd ..
+cd penglai-selinux-sdk && make && cp host/host $SECURE_WORLD_DIR/out/normal_linux/sdk/
 
 # build secure linux
 cd $SECURE_WORLD_DIR/starfive-secure-linux
+git checkout origin/origin_JH7100_VisionFive
+cd buildroot && git checkout JH7100_VisionFive_devel && cd ..
+cd HiFive_U-Boot && git checkout JH7100_VisionFive_devel && cd ..
+cd linux && git checkout origin/visionfive-5.15.y-devel && cd ..
+cd opensbi && git checkout master && cd ..
 make -j$CPU_NUM
-cp work/linux/arch/riscv/boot/Image $SECURE_WORLD_DIR/out/sec-image
-cp work/linux/arch/riscv/boot/dts/starfive/jh7100-starfive-visionfive-v1.dtb $SECURE_WORLD_DIR/out/sec-dtb.dtb
+make clean          # Now we use this special compile method, refine in the future
+git checkout main
+cd linux && git checkout 1d32875c3de21db25145967f467c754223cb901f && cd ..
+make -j$CPU_NUM
+cp work/linux/arch/riscv/boot/Image $SECURE_WORLD_DIR/out/secure_linux/sec-image
+cp work/linux/arch/riscv/boot/dts/starfive/jh7100-starfive-visionfive-v1.dtb $SECURE_WORLD_DIR/out/secure_linux/sec-dtb.dtb
+
+# generate grub.cfg and run.sh
+cat > $SECURE_WORLD_DIR/out/uboot/grub.cfg <<EOF
+set default=0
+set timeout_style=menu
+set timeout=2
+
+set debug="linux,loader,mm"
+set term="vt100"
+
+menuentry 'boot_by_UART1 plic_hart0 no_network Fedora_with_modules reserved_payload linux-5.15.10 visionfive' {
+    linux /fedora_with_modules.gz ro root=UUID=59fcd098-2f22-441a-ba45-4f7185baf23f rhgb console=tty0 console=ttyS2,115200 earlycon rootwait stmmaceth=chain_mode:1 selinux=0 LANG=en_US.UTF-8
+    devicetree /dtbs/5.15.10+/starfive/visionfive-v1-uart-reserved-plic-nonet.dtb
+        initrd /initramfs-5.15.10+.img
+}
+EOF
+
+cat > $SECURE_WORLD_DIR/out/riscv/run.sh <<EOF
+cd new_sec
+sudo insmod penglai_linux.ko
+./host sec-image sec-dtb.dtb
+EOF
--- script/install_all.sh
+++ script/install_all.sh
@@ -21,6 +21,8 @@ then
     echo 'Device not found!'
     exit 1
 fi
+BOOT_PARTITION=$DEVICE"3"
+ROOTFS_PARTITION=$DEVICE"4"
 
 if test -z "$SECURE_WORLD_DIR";then
 	echo "SECURE_WORLD_DIR is null string"
@@ -30,42 +32,48 @@ fi
 
 cd $SECURE_WORLD_DIR
 mkdir -p $SECURE_WORLD_DIR/disk
+mkdir -p $SECURE_WORLD_DIR/disk/__boot
+mkdir -p $SECURE_WORLD_DIR/disk/__
 
 # install normal linux
-sudo mount /dev/sda3 $SECURE_WORLD_DIR/disk/__boot
-sudo cat > $SECURE_WORLD_DIR/disk/__boot/grub.cfg <<EOF
-set default=0
-set timeout_style=menu
-set timeout=2
+sudo mount $BOOT_PARTITION $SECURE_WORLD_DIR/disk/__boot
+# sudo cat > $SECURE_WORLD_DIR/out/grub.cfg <<EOF
+# set default=0
+# set timeout_style=menu
+# set timeout=2
 
-set debug="linux,loader,mm"
-set term="vt100"
+# set debug="linux,loader,mm"
+# set term="vt100"
 
-menuentry 'boot_by_UART1 plic_hart0 no_network Fedora_with_modules reserved_payload linux-5.15.10 visionfive' {
-    linux /fedora_with_modules.gz ro root=UUID=59fcd098-2f22-441a-ba45-4f7185baf23f rhgb console=tty0 console=ttyS2,115200 earlycon rootwait stmmaceth=chain_mode:1 selinux=0 LANG=en_US.UTF-8
-    devicetree /dtbs/5.15.10+/starfive/visionfive-v1-uart-reserved-plic-nonet.dtb
-        initrd /initramfs-5.15.10+.img
-}
-EOF
-sudo cp out/fedora_with_modules.gz $SECURE_WORLD_DIR/disk/__boot/
-sudo cp out/visionfive-v1-uart-reserved-plic-nonet.dtb $SECURE_WORLD_DIR/disk/__boot/dtbs/5.15.10+/starfive/
+# menuentry 'boot_by_UART1 plic_hart0 no_network Fedora_with_modules reserved_payload linux-5.15.10 visionfive' {
+#     linux /fedora_with_modules.gz ro root=UUID=59fcd098-2f22-441a-ba45-4f7185baf23f rhgb console=tty0 console=ttyS2,115200 earlycon rootwait stmmaceth=chain_mode:1 selinux=0 LANG=en_US.UTF-8
+#     devicetree /dtbs/5.15.10+/starfive/visionfive-v1-uart-reserved-plic-nonet.dtb
+#         initrd /initramfs-5.15.10+.img
+# }
+# EOF
+sudo cp $SECURE_WORLD_DIR/out/uboot/grub.cfg $SECURE_WORLD_DIR/disk/__boot/
+sudo cp $SECURE_WORLD_DIR/out/normal_linux/kernel/fedora_with_modules.gz $SECURE_WORLD_DIR/disk/__boot/
+sudo cp $SECURE_WORLD_DIR/out/normal_linux/kernel/visionfive-v1-uart-reserved-plic-nonet.dtb $SECURE_WORLD_DIR/disk/__boot/dtbs/5.15.10+/starfive/
 sudo sync
-sudo umount $SECURE_WORLD_DIR/disk/__/
+sudo umount $SECURE_WORLD_DIR/disk/__boot/
 echo 'Transfer normal linux successfully!'
 
-# install driver and secure linux
-sudo mount /dev/sda4 $SECURE_WORLD_DIR/disk/__
-sudo cat > $SECURE_WORLD_DIR/disk/__/home/riscv/run.sh <<EOF
-cd new_sec
-sudo insmod penglai_linux.ko
-./host sec-image sec-dtb.dtb
-EOF
+# install sdk, driver and secure linux
+sudo mount $ROOTFS_PARTITION $SECURE_WORLD_DIR/disk/__
+# sudo cat > $SECURE_WORLD_DIR/out/riscv/run.sh <<EOF
+# cd new_sec
+# sudo insmod penglai_linux.ko
+# ./host sec-image sec-dtb.dtb
+# EOF
+sudo cp $SECURE_WORLD_DIR/out/normal_linux/sdk/run.sh $SECURE_WORLD_DIR/disk/__/home/riscv/run.sh
 sudo chmod a+x $SECURE_WORLD_DIR/disk/__/home/riscv/run.sh
+
 sudo mkdir -p $SECURE_WORLD_DIR/disk/__/home/riscv/new_sec
-sudo cp out/host $SECURE_WORLD_DIR/disk/__/home/riscv/new_sec/
-sudo cp out/penglai_linux.ko $SECURE_WORLD_DIR/disk/__/home/riscv/new_sec/
-sudo cp out/sec-image $SECURE_WORLD_DIR/disk/__/home/riscv/new_sec/
-sudo cp out/sec-dtb.dtb $SECURE_WORLD_DIR/disk/__/home/riscv/new_sec/
+sudo cp $SECURE_WORLD_DIR/out/normal_linux/sdk/host $SECURE_WORLD_DIR/disk/__/home/riscv/new_sec/
+sudo cp $SECURE_WORLD_DIR/out/normal_linux/driver/penglai_linux.ko $SECURE_WORLD_DIR/disk/__/home/riscv/new_sec/
+
+sudo cp $SECURE_WORLD_DIR/out/secure_linux/sec-image $SECURE_WORLD_DIR/disk/__/home/riscv/new_sec/
+sudo cp $SECURE_WORLD_DIR/out/secure_linux/sec-dtb.dtb $SECURE_WORLD_DIR/disk/__/home/riscv/new_sec/
 echo 'stat sec-image'
 sudo stat $SECURE_WORLD_DIR/disk/__/home/riscv/new_sec/sec-image
 echo 'stat sec-dtb.dtb'
