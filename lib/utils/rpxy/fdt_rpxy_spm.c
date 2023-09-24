1bc08d536d6703104a7aeb49065ad072dafbee75
Qingyu Shang <2931013282@sjtu.edu.cn>
2023-09-25 00:13:05 +0800

    set boot args when setup STMM in spm_mm initialization
    Signed-off-by: Qingyu Shang <2931013282@sjtu.edu.cn>

--- lib/utils/rpxy/fdt_rpxy_spm.c
+++ lib/utils/rpxy/fdt_rpxy_spm.c
@@ -108,6 +108,81 @@ static void spm_sp_synchronous_exit(sp_context_t *ctx, uint64_t rc)
 
 sp_context_t mm_context;
 
+typedef uint8_t UINT8;
+typedef uint16_t UINT16;
+typedef uint32_t UINT32;
+typedef uint64_t UINT64;
+
+typedef struct {
+	UINT8 Type;	/* type of the structure */
+	UINT8 Version; /* version of this structure */
+	UINT16 Size;   /* size of this structure in bytes */
+	UINT32 Attr;   /* attributes: unused bits SBZ */
+} EFI_PARAM_HEADER;
+
+typedef struct {
+	UINT64 Mpidr;
+	UINT32 LinearId;
+	UINT32 Flags;
+} EFI_SECURE_PARTITION_CPU_INFO;
+
+typedef struct {
+	EFI_PARAM_HEADER Header;
+	UINT64 SpMemBase;
+	UINT64 SpMemLimit;
+	UINT64 SpImageBase;
+	UINT64 SpStackBase;
+	UINT64 SpHeapBase;
+	UINT64 SpNsCommBufBase;
+	UINT64 SpSharedBufBase;
+	UINT64 SpImageSize;
+	UINT64 SpPcpuStackSize;
+	UINT64 SpHeapSize;
+	UINT64 SpNsCommBufSize;
+	UINT64 SpSharedBufSize;
+	UINT32 NumSpMemRegions;
+	UINT32 NumCpus;
+	EFI_SECURE_PARTITION_CPU_INFO *CpuInfo;
+} EFI_SECURE_PARTITION_BOOT_INFO;
+
+void (*_SMM_ModuleInit)(UINT64 CpuId, void *BootInfoAddress);
+
+typedef struct {
+	EFI_SECURE_PARTITION_BOOT_INFO MmPayloadBootInfo;
+	EFI_SECURE_PARTITION_CPU_INFO MmCpuInfo[1];
+} EFI_SECURE_SHARED_BUFFER;
+
+void set_boot_args(struct sbi_trap_regs *regs)
+{
+	sbi_printf("entry set_boot_args\n");
+
+	EFI_SECURE_SHARED_BUFFER *MmSharedBuffer = (EFI_SECURE_SHARED_BUFFER *)0x80B00000;
+
+	MmSharedBuffer->MmPayloadBootInfo.Header.Version = 0x01;
+	MmSharedBuffer->MmPayloadBootInfo.SpMemBase	= 0x80C00000;
+	MmSharedBuffer->MmPayloadBootInfo.SpMemLimit	= 0x82000000;
+	MmSharedBuffer->MmPayloadBootInfo.SpImageBase = 0x80C00000; // SpMemBase
+	MmSharedBuffer->MmPayloadBootInfo.SpStackBase =
+		0x81FFFFFF; // SpHeapBase + SpHeapSize + SpStackSize
+	MmSharedBuffer->MmPayloadBootInfo.SpHeapBase =
+		0x80F00000; // SpMemBase + SpImageSize
+	MmSharedBuffer->MmPayloadBootInfo.SpNsCommBufBase = 0xFFE00000;
+	MmSharedBuffer->MmPayloadBootInfo.SpSharedBufBase = 0x81F80000;
+	MmSharedBuffer->MmPayloadBootInfo.SpImageSize	 = 0x300000;
+	MmSharedBuffer->MmPayloadBootInfo.SpPcpuStackSize = 0x10000;
+	MmSharedBuffer->MmPayloadBootInfo.SpHeapSize	 = 0x800000;
+	MmSharedBuffer->MmPayloadBootInfo.SpNsCommBufSize = 0x200000;
+	MmSharedBuffer->MmPayloadBootInfo.SpSharedBufSize = 0x80000;
+	MmSharedBuffer->MmPayloadBootInfo.NumSpMemRegions = 0x6;
+	MmSharedBuffer->MmPayloadBootInfo.NumCpus	 = 1;
+	MmSharedBuffer->MmCpuInfo[0].LinearId		 = 0;
+	MmSharedBuffer->MmCpuInfo[0].Flags		 = 0;
+	MmSharedBuffer->MmPayloadBootInfo.CpuInfo = MmSharedBuffer->MmCpuInfo;
+
+    regs->a0 = current_hartid();
+	regs->a1 = (uintptr_t)&MmSharedBuffer->MmPayloadBootInfo;
+}
+
 int spm_mm_init()
 {
     int rc;
@@ -126,9 +201,8 @@ int spm_mm_init()
     mm_context.regs.mstatus = val;
     mm_context.regs.mepc = 0x80C00000;
 
-    // pass parameters
-	mm_context.regs.a0 = current_hartid();
-	mm_context.regs.a1 = 0;
+    // set boot arguments
+    set_boot_args(&mm_context.regs);
 
     // init secure CSR context
     mm_context.csr_stvec = 0x80C00000;
@@ -139,9 +213,6 @@ int spm_mm_init()
     __asm__ __volatile__("sfence.vma" : : : "memory");
 
     rc = spm_sp_synchronous_entry(&mm_context);
-    // register unsigned long a0 asm("a0") = current_hartid();
-	// register unsigned long a1 asm("a1") = 0;
-    // __asm__ __volatile__("mret" : : "r"(a0), "r"(a1));
 
     sbi_printf("spm_mm_init finish, retval: %d\n", rc);
     return rc;
@@ -208,9 +279,6 @@ static int rpxy_spm_init(void *fdt, int nodeoff,
     sbi_printf("rpxy_spm mm_group registered\n");
 
     spm_mm_init();
-
-    sbi_printf("rpxy_spm may I re entry now?\n");
-    spm_sp_synchronous_entry(&mm_context);
 
 	return 0;
 }
